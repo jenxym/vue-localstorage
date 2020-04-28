0149f39835c2ce27157c19cc006ca257e5baf6ac
Jennifer Morales <jemoraldi@gmail.com>
2020-04-28 13:43:48 +0100

    allow character with accents in form

--- src/components/TaskSetter.vue
+++ src/components/TaskSetter.vue
@@ -1,38 +1,38 @@
 <template>
-	<v-container>
-		<h2 class=".title mt-3">Add a new task</h2>
-		<v-card flat class="pa-4 grey lighten-4">
-			<v-form v-model="valid" ref="form">
-				<v-text-field outlined required v-model="taskData.taskName" :rules="nameRules" label="Add a title"></v-text-field>
-				<v-text-field outlined required v-model="taskData.taskInfo" :rules="infoRules" label="Add a short description"></v-text-field>
-				<v-btn depressed tile large color="success" @click="addTask"><v-icon>mdi-plus</v-icon>Add task</v-btn>
-			</v-form>
-		</v-card>
-	</v-container>
+  <v-container>
+    <h2 class=".title mt-3">Add a new task</h2>
+    <v-card flat class="pa-4 grey lighten-4">
+      <v-form v-model="valid" ref="form">
+        <v-text-field outlined required v-model="taskData.taskName" :rules="nameRules" label="Add a title"></v-text-field>
+        <v-text-field outlined required v-model="taskData.taskInfo" :rules="infoRules" label="Add a short description"></v-text-field>
+        <v-btn depressed tile large color="success" @click="addTask"><v-icon>mdi-plus</v-icon>Add task</v-btn>
+      </v-form>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
-import { EventBus } from '@/events/event-bus.js';
+import { EventBus } from "@/events/event-bus.js";
 export default {
-	data() {
-		return {
-			valid: false,
-			nameRules: [(v) => !!v || 'A task name is required', (v) => /^[a-zA-Z0-9 ]+$/.test(v) || 'Only alphanumerics characters'],
-			infoRules: [(v) => !!v || 'A task description is required', (v) => /^[a-zA-Z0-9 ]+$/.test(v) || 'Only alphanumerics characters'],
-			taskData: {
-				taskName: '',
-				taskInfo: '',
-			},
-		};
-	},
-	methods: {
-		addTask() {
-			if (!this.$refs.form.validate()) return;
-			EventBus.$emit('addtask', this.taskData);
-			this.taskData = {};
-			this.$refs.form.reset();
-		},
-	},
+  data() {
+    return {
+      valid: false,
+      nameRules: [(v) => !!v || "A task name is required", (v) => /^[a-zA-Z0-9\u00C0-\u00FF ]+$/.test(v) || "Only alphanumerics characters"],
+      infoRules: [(v) => !!v || "A task description is required", (v) => /^[a-zA-Z0-9\u00C0-\u00FF ]+$/.test(v) || "Only alphanumerics characters"],
+      taskData: {
+        taskName: "",
+        taskInfo: "",
+      },
+    };
+  },
+  methods: {
+    addTask() {
+      if (!this.$refs.form.validate()) return;
+      EventBus.$emit("addtask", this.taskData);
+      this.taskData = {};
+      this.$refs.form.reset();
+    },
+  },
 };
 </script>
 
